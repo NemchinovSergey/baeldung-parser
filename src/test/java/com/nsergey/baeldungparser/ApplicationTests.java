2505b5616d41a7160485dc205ef67fec1672f657
Sergey Nemchinov <googoodemon@gmail.com>
2019-02-09 22:26:52 +0700

    class renamed

--- src/test/java/com/nsergey/baeldungparser/ApplicationTests.java
+++ src/test/java/com/nsergey/baeldungparser/ApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BaeldungParserApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
