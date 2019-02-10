0a2d1ea33bbb99a22185759d1aa5747465b168bc
Sergey Nemchinov <googoodemon@gmail.com>
2019-02-10 22:31:03 +0700

    Application class refactoring

--- src/main/java/com/nsergey/baeldungparser/Application.java
+++ src/main/java/com/nsergey/baeldungparser/Application.java
@@ -3,30 +3,29 @@ package com.nsergey.baeldungparser;
 import com.nsergey.baeldungparser.extractor.BaeldungExtractor;
 import com.nsergey.baeldungparser.parser.DocumentParser;
 import org.jsoup.nodes.Document;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
-
-    @Autowired
-    private BaeldungExtractor extractor;
-
-    @Autowired
-    private DocumentParser parser;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 	@EventListener(ApplicationReadyEvent.class)
-	public void start() {
+	public void start(ApplicationReadyEvent event) {
+        ConfigurableApplicationContext ctx = event.getApplicationContext();
+
+        BaeldungExtractor extractor = ctx.getBean(BaeldungExtractor.class);
+        DocumentParser parser = ctx.getBean(DocumentParser.class);
+
         Document document = extractor.loadDocument("https://www.baeldung.com/java-with-jsoup");
         if (document != null) {
-            parser.parseDocument(document);
+            parser.parse(document);
         }
     }
 
