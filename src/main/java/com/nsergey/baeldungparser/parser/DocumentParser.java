ce1aa840e4aa63b3eec71d186ae0466192c018af
Sergey Nemchinov <googoodemon@gmail.com>
2019-02-10 22:30:41 +0700

    DocumentParser refactoring

--- src/main/java/com/nsergey/baeldungparser/parser/DocumentParser.java
+++ src/main/java/com/nsergey/baeldungparser/parser/DocumentParser.java
@@ -1,42 +1,47 @@
 package com.nsergey.baeldungparser.parser;
 
-import com.vladsch.flexmark.convert.html.FlexmarkHtmlParser;
+import com.nsergey.baeldungparser.render.Render;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class DocumentParser {
 
+    private final Render render;
+
+    @Autowired
+    public DocumentParser(Render render) {
+        this.render = render;
+    }
+
     /**
      * https://jsoup.org/apidocs/org/jsoup/select/Selector.html
      */
-    public void parseDocument(Document doc) {
+    public void parse(Document doc) {
         System.out.println("Header:");
         Element headerH1 = doc.select("header h1")
                               .first();
         System.out.println(String.format("\"%s\"\n", headerH1.text()));
 
         Elements article = doc.select("section[itemprop=\"articleBody\"]");
-        article.forEach(this::renderToMarkdown);
-    }
+        assert article.size() == 1;
 
-    private void renderToMarkdown(Element element) {
-        renderToMarkdown(element.toString());
-    }
-
-    private String renderToMarkdown(String html) {
-        String markdown = FlexmarkHtmlParser.parse(html);
-
-        System.out.println("HTML:");
-        System.out.println(html);
-
-        System.out.println("\nMarkdown:");
-        System.out.println(markdown);
-        return markdown;
+        List<String> list = article.first()
+                                      .children()
+                                      .stream()
+                                      .peek(element -> log.info("HTML: {}", element.outerHtml()))
+                                      .map(render::render)
+                                      .peek(markdown -> log.info("Markdown: {}", markdown))
+                                      .collect(Collectors.toList());
+        //list.forEach(log::info);
     }
 
 }
