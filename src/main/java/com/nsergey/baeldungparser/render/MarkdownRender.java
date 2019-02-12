b0c9d368d1cd989d64fedf4cdf8aa2f52933c082
Sergey Nemchinov <googoodemon@gmail.com>
2019-02-12 21:39:36 +0700

    Markdown parser debugging

--- src/main/java/com/nsergey/baeldungparser/render/MarkdownRender.java
+++ src/main/java/com/nsergey/baeldungparser/render/MarkdownRender.java
@@ -1,13 +1,18 @@
 package com.nsergey.baeldungparser.render;
 
 import com.vladsch.flexmark.convert.html.FlexmarkHtmlParser;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.nodes.Element;
 
+@Slf4j
 public class MarkdownRender implements Render {
 
     @Override
     public String render(Element element) {
-        return FlexmarkHtmlParser.parse(element.outerHtml());
+        log.debug("HTML: {}", element.outerHtml());
+        String md = FlexmarkHtmlParser.parse(element.outerHtml());
+        log.debug("Markdown: {}", md);
+        return md;
     }
 
 }
