dc32b64586902f7d8c54039014a99cc041b40fbd
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-07 18:46:14 +0700

    Added some comment

--- src/main/java/com/nsergey/baeldungparser/render/WordPressRender.java
+++ src/main/java/com/nsergey/baeldungparser/render/WordPressRender.java
@@ -6,6 +6,7 @@ public class WordPressRender implements Render {
 
     @Override
     public String render(Element element) {
+        // by default the all blocks are java
         return null;
     }
 }
