a769e799b03f512bf67aec7acda1a8ee5ec2de13
Nemchinov Sergey <googoodemon@gmail.com>
2019-09-07 22:19:11 +0700

    created an application to download file from page

---
+++ src/main/java/com/nsergey/quickdownloader/Application.java
@@ -0,0 +1,160 @@
+package com.nsergey.quickdownloader;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+
+public class Application {
+
+    private static List<String> DOWNLOADABLE = Arrays.asList("zip", "mp3", "avi", "pdf", "mov", "m4v", "mp4");
+
+    private static DownloadFileHttpClient httpClient = new DownloadFileHttpClient();
+
+	public static void main(String[] args) throws IOException {
+        if (args.length < 2) {
+            showUsageHelp();
+            return;
+        }
+
+        String startingPage = args[0];
+        Path dir = Paths.get(args[1]);
+        createDirectory(dir);
+
+        Document document = loadPageDocument(startingPage);
+        if (document != null) {
+            Elements body = getBody(document);
+            //saveElementsToFile(dir + "\\index.html", body);
+
+            Elements links = getLinks(body);
+            int successNumber = 1;
+            for (int i = 0; i < links.size(); i++) {
+                Element e = links.get(i);
+                int count = processPage(e.text(), e.attr("href"), dir, successNumber);
+                if (count > 0) {
+                    successNumber++;
+                }
+            }
+        }
+	}
+
+    private static int processPage(String title, String url, Path dir, int number) {
+        System.out.println(String.format("Start Page: [%s] -> %s", title, url));
+        int count = 0;
+        try {
+            Path pageDir = dir.resolve(buildPageDirectoryName(title, number));
+
+            Document page = loadPageDocument(url);
+            Elements links = page.select("a");
+            for (Element link : links) {
+                if (processLink(pageDir, link)) {
+                    count++;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception while page loading: " + e.getMessage());
+        } finally {
+            System.out.println(String.format("Finished page: [%s]", title));
+        }
+        return count;
+    }
+
+    private static boolean processLink(Path pageDir, Element link) {
+        String fileUrl = link.attr("href");
+        System.out.println("Process link: " + link);
+        try {
+            String fileNameOnly = Paths.get(new URL(fileUrl).getFile()).getFileName().toString();
+            if (isUrlDownloadable(fileNameOnly)) {
+                createDirectory(pageDir);
+                Path saveToFileName = pageDir.resolve(fileNameOnly);
+                long bytes = httpClient.download(fileUrl, saveToFileName.toString());
+                if (bytes > 0) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Exception while link processing: " + e.getMessage());
+        }
+        return false;
+    }
+
+    private static boolean isUrlDownloadable(String fileNameOnly) {
+        System.out.println("URL: " + fileNameOnly);
+        String extension = getFileNameExtension(fileNameOnly).orElse(null);
+        System.out.println("File ext: " + extension);
+        if (extension != null && !"".equals(extension)) {
+            return DOWNLOADABLE.contains(extension.toLowerCase());
+        }
+        return false;
+    }
+
+    private static Optional<String> getFileNameExtension(String filename) {
+        return Optional.ofNullable(filename)
+                       .filter(f -> f.contains("."))
+                       .map(f -> f.substring(filename.lastIndexOf(".") + 1));
+    }
+
+    private static void showUsageHelp() {
+        System.out.println("Usage: java com.nsergey.quickdownloader.Application url directory");
+        System.out.println("Exit...");
+    }
+
+    private static Document loadPageDocument(String startingPage) throws IOException {
+        return Jsoup.connect(startingPage).get();
+    }
+
+    private static String buildPageDirectoryName(String title, int number) {
+        return String.format("%d. %s", number, filterDirectoryName(title));
+    }
+
+    private static String filterDirectoryName(String title) {
+        return title.replace('/', '-');
+    }
+
+    private static void createDirectory(Path dir) throws IOException {
+        if (!(Files.exists(dir) && Files.isDirectory(dir))) {
+            Files.createDirectories(dir);
+        }
+    }
+
+    private static void saveElementsToFile(String fileName, Elements body) {
+        List<String> strings = body.first()
+                                      .children()
+                                      .stream()
+                                      .map(Node::outerHtml)
+                                      .collect(Collectors.toList());
+
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileName),
+                                                             StandardOpenOption.CREATE)) {
+            for (String s : strings) {
+                writer.write(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Elements getBody(Document document) {
+        return document.select("div .post-body");
+    }
+
+    private static Elements getLinks(Elements body) {
+        return body.select("a");
+    }
+
+
+}
+
